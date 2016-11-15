75376abd63a0cac1300b830d552e0f28cfaa2e47
Vincent Guyader <Vincent Guyader>
2016-11-15 12:55:22 +0100

    Correction fichier JS

--- src/site/private_html/js/darties.js
+++ src/site/private_html/js/darties.js
@@ -163,3 +163,19 @@ function getFiltresData($) {
 		'I_REGION' 	  : $('#I_REGION').val(),
 	};
 }
+
+/*
+* Function activated when button show password is being pressed
+*/
+function toggleText(id){
+    var inputPasswd = document.getElementById("inputPasswd"+id);
+    inputPasswd.type = "text";
+}
+   
+/*
+* Function activated when button show password is being released
+*/
+function togglePasswd(id){
+    var inputPasswd = document.getElementById("inputPasswd"+id);
+    inputPasswd.type = "password"; 
+}
